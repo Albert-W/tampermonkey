eb24814834c3049d44ce2f1bea5dd4f751b948fb
Albert <wuyichang1990@gmail.com>
2019-09-11 15:23:09 +0100

    change the version

--- shanbayV2.js
+++ shanbayV2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shanbay display
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://web.shanbay.com/wordsweb/
