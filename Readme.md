6317f9d7f64e8b1079294df939d30461e771a55a
Yichang Wu <yichangw1@vmware.com>
2024-07-31 16:40:26 +0100

    update driving mock test

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# It's a repo host all of my tampermonkey scripts 
+
+## How to publish your script 
+
+1. Sign in greasyfork.org 
+
+2. Click your name 
+
+3. Click `Publish a script you've written`
+
+
--- driving-mock-test.js
+++ driving-mock-test.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Shortcut for Driving Mock Test
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  The script will automatically click on the start button and answer the questions for the driving mock test
+// @version      0.0.2
+// @description  The script will automatically click on the answers of the questions for the driving mock test
 // @author       You
 // @match        http://theory-tester.com/questions/*
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
@@ -15,7 +16,7 @@
     document.body.addEventListener('keydown',event => {
         var options = document.querySelectorAll("li.options-single label"); // Query all the <label> elements under <li> elements with class "options-single"
         var next = document.querySelector("span.icon-right-open")
-        var save = document.querySelector("span.icon-bookmark")  
+        var save = document.querySelector("button#save")  
 
         switch (event.key){
 
